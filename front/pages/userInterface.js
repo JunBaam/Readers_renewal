de382825f28651718706fe4495023eed4d211311
Jun <fkdlsl9088@gmail.com>
2021-03-03 13:20:00 +0900

    [test] 적용테스트

--- front/pages/userInterface.js
+++ front/pages/userInterface.js
@@ -2,7 +2,7 @@ import React from 'react'
 import AppLayout from '../components/Layout/AppLayout'
 
 const userInterface = () => {
-  return <AppLayout>로그인회원가입</AppLayout>
+  return <AppLayout>로그인회원가입dd</AppLayout>
 }
 
 export default userInterface
